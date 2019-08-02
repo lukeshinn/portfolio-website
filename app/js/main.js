e3f24d97aee67435cfb1eeaf71621920c2bc15d8
lukeshinn <luke.shinn.efp@gmail.com>
2019-08-02 15:34:17 -0500

    :hammer: - add delta wheel value function

--- app/js/main.js
+++ app/js/main.js
@@ -9,124 +9,124 @@
  * http://www.codrops.com
  */
 {
-	const DOM = {};
-	DOM.intro = document.querySelector('.content--intro');
-	DOM.shape = DOM.intro.querySelector('svg.shape');
-	DOM.path = DOM.shape.querySelector('path');
-	DOM.enter = document.querySelector('.enter');
-	DOM.main= document.getElementById('main');
-	charming(DOM.enter);
-	DOM.enterLetters = Array.from(DOM.enter.querySelectorAll('span'));
-	// Set the SVG transform origin.
-	DOM.shape.style.transformOrigin = '50% 0%';
+  const DOM = {};
+  DOM.intro = document.querySelector('.content--intro');
+  DOM.shape = DOM.intro.querySelector('svg.shape');
+  DOM.path = DOM.shape.querySelector('path');
+  DOM.enter = document.querySelector('.enter');
+  DOM.main= document.getElementById('main');
+  charming(DOM.enter);
+  DOM.enterLetters = Array.from(DOM.enter.querySelectorAll('span'));
+  // Set the SVG transform origin.
+  DOM.shape.style.transformOrigin = '50% 0%';
 
-	const init = () => {
-		imagesLoaded(document.body, {background: true} , () => document.body.classList.remove('loading'));
-		DOM.enter.addEventListener('click', navigate);
-		DOM.enter.addEventListener('touchenter', navigate);
-		DOM.enter.addEventListener('mouseenter', enterHoverInFn);
-		DOM.enter.addEventListener('mouseleave', enterHoverOutFn);
-	};
+  const init = () => {
+    imagesLoaded(document.body, {background: true} , () => document.body.classList.remove('loading'));
+    DOM.enter.addEventListener('click', navigate);
+    DOM.enter.addEventListener('touchenter', navigate);
+    DOM.enter.addEventListener('mouseenter', enterHoverInFn);
+    DOM.enter.addEventListener('mouseleave', enterHoverOutFn);
+  };
 
-	let loaded;
-	const navigate = () => {
-		if ( loaded ) return;
-		loaded = true;
+  let loaded;
+  const navigate = () => {
+    if ( loaded ) return;
+    loaded = true;
 
-		anime({
-			targets: DOM.intro,
-			duration: 1100,
-			easing: 'easeInOutSine',
-			translateY: '-200vh'
-		});
+    anime({
+      targets: DOM.intro,
+      duration: 1100,
+      easing: 'easeInOutSine',
+      translateY: '-200vh'
+    });
 
-		anime({
-			targets: DOM.shape,
-			scaleY: [
-				{value:[0.8,1.8],duration: 550,easing: 'easeInQuad'},
-				{value:1,duration: 550,easing: 'easeOutQuad'}
-			]
-		});
+    anime({
+      targets: DOM.shape,
+      scaleY: [
+        {value:[0.8,1.8],duration: 550,easing: 'easeInQuad'},
+        {value:1,duration: 550,easing: 'easeOutQuad'}
+      ]
+    });
 
-		anime({
-			targets: DOM.path,
-			duration: 1100,
-			easing: 'easeOutQuad',
-			d: DOM.path.getAttribute('pathdata:id')
-		});
-		main.classList.add("auto-height");
-	};
+    anime({
+      targets: DOM.path,
+      duration: 1100,
+      easing: 'easeOutQuad',
+      d: DOM.path.getAttribute('pathdata:id')
+    });
+    main.classList.add("auto-height");
+  };
 
-	let isActive;
-	let enterTimeout;
+  let isActive;
+  let enterTimeout;
 
-	const enterHoverInFn = () => enterTimeout = setTimeout(() => {
-		isActive = true;
-		anime.remove(DOM.enterLetters);
-		anime({
-			targets: DOM.enterLetters,
-			delay: (t,i) => i*15,
-			translateY: [
-				{value: 10, duration: 150, easing: 'easeInQuad'},
-				{value: [-10,0], duration: 150, easing: 'easeOutQuad'}
-			],
-			opacity: [
-				{value: 0, duration: 150, easing: 'linear'},
-				{value: 1, duration: 150, easing: 'linear'}
-			],
-			color: {
-				value: '#ff963b',
-				duration: 1,
-				delay: (t,i,l) => i*15+150
-			}
-		});
-	}, 50);
+  const enterHoverInFn = () => enterTimeout = setTimeout(() => {
+    isActive = true;
+    anime.remove(DOM.enterLetters);
+    anime({
+      targets: DOM.enterLetters,
+      delay: (t,i) => i*15,
+      translateY: [
+        {value: 10, duration: 150, easing: 'easeInQuad'},
+        {value: [-10,0], duration: 150, easing: 'easeOutQuad'}
+      ],
+      opacity: [
+        {value: 0, duration: 150, easing: 'linear'},
+        {value: 1, duration: 150, easing: 'linear'}
+      ],
+      color: {
+        value: '#ff963b',
+        duration: 1,
+        delay: (t,i,l) => i*15+150
+      }
+    });
+  }, 50);
 
-	const enterHoverOutFn = () => {
-		clearTimeout(enterTimeout);
-		if( !isActive ) return;
-		isActive = false;
+  const enterHoverOutFn = () => {
+    clearTimeout(enterTimeout);
+    if( !isActive ) return;
+    isActive = false;
 
-		anime.remove(DOM.enterLetters);
-		anime({
-			targets: DOM.enterLetters,
-			delay: (t,i,l) => (l-i-1)*15,
-			translateY: [
-				{value: 10, duration: 150, easing: 'easeInQuad'},
-				{value: [-10,0], duration: 150, easing: 'easeOutQuad'}
-			],
-			opacity: [
-				{value: 0, duration: 150, easing: 'linear'},
-				{value: 1, duration: 150, easing: 'linear'}
-			],
-			color: {
-				value: '#ffffff',
-				duration: 1,
-				delay: (t,i,l) => (l-i-1)*15+150
-			}
-		});
-	};
+    anime.remove(DOM.enterLetters);
+    anime({
+      targets: DOM.enterLetters,
+      delay: (t,i,l) => (l-i-1)*15,
+      translateY: [
+        {value: 10, duration: 150, easing: 'easeInQuad'},
+        {value: [-10,0], duration: 150, easing: 'easeOutQuad'}
+      ],
+      opacity: [
+        {value: 0, duration: 150, easing: 'linear'},
+        {value: 1, duration: 150, easing: 'linear'}
+      ],
+      color: {
+        value: '#ffffff',
+        duration: 1,
+        delay: (t,i,l) => (l-i-1)*15+150
+      }
+    });
+  };
 
-	init();
+  init();
 }
 
 // =========================================================
 ///  Vue
 // =========================================================
 /*Vue.component('todo-item', {*/
-  //props: ['todo'],
-  //template: '<li>{{ todo.text }}</li>'
+//props: ['todo'],
+//template: '<li>{{ todo.text }}</li>'
 //});
 
 //var app7 = new Vue({
-  //el: '#app',
-  //data: {
-    //groceryList: [
-      //{ id: 0, text: 'Vegetables' },
-      //{ id: 1, text: 'Cheese' },
-      //{ id: 2, text: 'Whatever else humans are supposed to eat' }
-    //]
-  //}
+//el: '#app',
+//data: {
+//groceryList: [
+//{ id: 0, text: 'Vegetables' },
+//{ id: 1, text: 'Cheese' },
+//{ id: 2, text: 'Whatever else humans are supposed to eat' }
+//]
+//}
 //});
 
 // scroll indicator component
@@ -140,6 +140,16 @@ Vue.component('indicator', {
       var docHeight = document.documentElement.scrollHeight // instead document.body.clientHeight
       var perc = 100 * scrollPos / (docHeight - winHeight)
       vm.$el.style.width = perc + '%'
+    })
+
+    var currentPos = 0;
+    const calculate  = (e) => {
+      currentPos += e.deltaY;
+      vm.$el.style.width = currentPos + '%'
+    };
+
+    window.addEventListener("wheel", (e) => {
+      calculate(e);
     })
 
   }
