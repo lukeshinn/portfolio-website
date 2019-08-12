c13bf92a42ae681321640fd4440f102bd988a731
lukeshinn <luke.shinn.efp@gmail.com>
2019-08-12 15:05:36 -0500

    :hammer: - Build out intro animation

--- app/js/main.js
+++ app/js/main.js
@@ -1,50 +1,86 @@
 /**
- * demo1.js
- * http://www.codrops.com
+ * portfolio-website
+ * http://www.lukeshinn.com
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
  *
- * Copyright 2017, Codrops
- * http://www.codrops.com
+ * Copyright 2019, Luke Shinn
+ * http://www.lukeshinn.com
  */
 
 
 {
+    const introTextLine1 = document.body.querySelector(".loader__text-line--1 > div")
+    const introTextLine2 = document.body.querySelector(".loader__text-line--2 > div")
+    const introBackground = document.body.querySelector(".loader")
+    const box3 = document.body.querySelector("div.box3")
+
 
   const init = () => {
+    introductionTextController()
+  };
 
-    const box = document.body.querySelector("div.box")
-    const box2 = document.body.querySelector("div.box2")
-    const box3 = document.body.querySelector("div.box3")
-    let currentState = 0
+  // Introduction Text
+  // #####################################
+  // this controller is responsible for the animations
+  // of the first view the user sees which is a black screen
+  // and some hidden text
+    const introductionTextController = () => {
 
+      // keyframes
+      var keyframes__intro_bg = [
+        { transform: 'translateX(0)' },
+        { transform: 'translateY(-100%)' }
+      ];
 
-    const animateToNextState = () => {
+      var keyframes__intro_words = [
+        { transform: 'matrix(1.00,0.07,-0.07,1.00,0,75)' },
+        { transform: 'matrix(1,0,0,1,0,0)' },
+        { transform: 'matrix(1,0,0,1,0,0)' },
+        { transform: 'matrix(1.00,-0.03,0.03,1.00,0,-75)' }
+      ];
 
-      var tl = anime.timeline({
-        easing: 'easeInOutBounce',
-        duration: 10000
-      });
+      // duration
+      var timimg__intro_bg = {
+        duration: 1000,
+        iterations: 1,
+        fill: 'forwards'
+      }
 
-      tl
-        .add({
-          targets: box,
-          translateX: 250,
-        })
-        .add({
-          targets: box2,
-          translateX: 250,
-        })
-        .add({
-          targets: box3,
-          translateX: 250,
-        }, '+=1000');
+      var timimg__intro_word = {
+        duration: 2000,
+        iterations: 1,
+        fill: 'forwards'
+      }
+
+      // animation variables
+      var introBg = introBackground.animate(
+        keyframes__intro_bg,
+        timimg__intro_bg
+      )
+
+      var introLine1 = introTextLine1.animate(
+        keyframes__intro_words,
+        timimg__intro_word
+      )
+
+      var introLine2 = introTextLine2.animate(
+        keyframes__intro_words,
+        timimg__intro_word
+      )
+
+      introLine1.play()
+      introLine2.pause()
+      introBg.pause()
+      introLine1.onfinish = function() {
+        setTimeout(function(){ introLine2.play() }, 0)
+      }
+      introLine2.onfinish = function() {
+        setTimeout(function(){ introBg.play() }, 0)
+      }
     }
 
-    animateToNextState()
-
-  };
 
   init();
 }
@@ -70,6 +106,7 @@ Vue.component('indicator', {
     const calculate  = (e) => {
       currentPos += e.deltaY;
       vm.$el.style.width = currentPos + '%'
+      console.log(currentPos);
     };
 
     window.addEventListener("wheel", (e) => {
@@ -89,104 +126,104 @@ Vue.config.devtools = true;
 //  Old
 // #####################################
 
-{
-  const DOM = {};
-  DOM.intro = document.querySelector('.content--intro');
-  DOM.shape = DOM.intro.querySelector('svg.shape');
-  DOM.path = DOM.shape.querySelector('path');
-  DOM.enter = document.querySelector('.enter');
-  DOM.main= document.getElementById('main');
-  charming(DOM.enter);
-  DOM.enterLetters = Array.from(DOM.enter.querySelectorAll('span'));
-  // Set the SVG transform origin.
-  DOM.shape.style.transformOrigin = '50% 0%';
+/*{*/
+//const DOM = {};
+//DOM.intro = document.querySelector('.content--intro');
+//DOM.shape = DOM.intro.querySelector('svg.shape');
+//DOM.path = DOM.shape.querySelector('path');
+//DOM.enter = document.querySelector('.enter');
+//DOM.main= document.getElementById('main');
+//charming(DOM.enter);
+//DOM.enterLetters = Array.from(DOM.enter.querySelectorAll('span'));
+//// Set the SVG transform origin.
+//DOM.shape.style.transformOrigin = '50% 0%';
 
-  const init = () => {
-    imagesLoaded(document.body, {background: true} , () => document.body.classList.remove('loading'));
-    DOM.enter.addEventListener('click', navigate);
-    DOM.enter.addEventListener('touchenter', navigate);
-    DOM.enter.addEventListener('mouseenter', enterHoverInFn);
-    DOM.enter.addEventListener('mouseleave', enterHoverOutFn);
-  };
+//const init = () => {
+//imagesLoaded(document.body, {background: true} , () => document.body.classList.remove('loading'));
+//DOM.enter.addEventListener('click', navigate);
+//DOM.enter.addEventListener('touchenter', navigate);
+//DOM.enter.addEventListener('mouseenter', enterHoverInFn);
+//DOM.enter.addEventListener('mouseleave', enterHoverOutFn);
+//};
 
-  let loaded;
-  const navigate = () => {
-    if ( loaded ) return;
-    loaded = true;
+//let loaded;
+//const navigate = () => {
+//if ( loaded ) return;
+//loaded = true;
 
-    anime({
-      targets: DOM.intro,
-      duration: 1100,
-      easing: 'easeInOutSine',
-      translateY: '-200vh'
-    });
+//anime({
+//targets: DOM.intro,
+//duration: 1100,
+//easing: 'easeInOutSine',
+//translateY: '-200vh'
+//});
 
-    anime({
-      targets: DOM.shape,
-      scaleY: [
-        {value:[0.8,1.8],duration: 550,easing: 'easeInQuad'},
-        {value:1,duration: 550,easing: 'easeOutQuad'}
-      ]
-    });
+//anime({
+//targets: DOM.shape,
+//scaleY: [
+//{value:[0.8,1.8],duration: 550,easing: 'easeInQuad'},
+//{value:1,duration: 550,easing: 'easeOutQuad'}
+//]
+//});
 
-    anime({
-      targets: DOM.path,
-      duration: 1100,
-      easing: 'easeOutQuad',
-      d: DOM.path.getAttribute('pathdata:id')
-    });
-    main.classList.add("auto-height");
-  };
+//anime({
+//targets: DOM.path,
+//duration: 1100,
+//easing: 'easeOutQuad',
+//d: DOM.path.getAttribute('pathdata:id')
+//});
+//main.classList.add("auto-height");
+//};
 
-  let isActive;
-  let enterTimeout;
+//let isActive;
+//let enterTimeout;
 
-  const enterHoverInFn = () => enterTimeout = setTimeout(() => {
-    isActive = true;
-    anime.remove(DOM.enterLetters);
-    anime({
-      targets: DOM.enterLetters,
-      delay: (t,i) => i*15,
-      translateY: [
-        {value: 10, duration: 150, easing: 'easeInQuad'},
-        {value: [-10,0], duration: 150, easing: 'easeOutQuad'}
-      ],
-      opacity: [
-        {value: 0, duration: 150, easing: 'linear'},
-        {value: 1, duration: 150, easing: 'linear'}
-      ],
-      color: {
-        value: '#ff963b',
-        duration: 1,
-        delay: (t,i,l) => i*15+150
-      }
-    });
-  }, 50);
+//const enterHoverInFn = () => enterTimeout = setTimeout(() => {
+//isActive = true;
+//anime.remove(DOM.enterLetters);
+//anime({
+//targets: DOM.enterLetters,
+//delay: (t,i) => i*15,
+//translateY: [
+//{value: 10, duration: 150, easing: 'easeInQuad'},
+//{value: [-10,0], duration: 150, easing: 'easeOutQuad'}
+//],
+//opacity: [
+//{value: 0, duration: 150, easing: 'linear'},
+//{value: 1, duration: 150, easing: 'linear'}
+//],
+//color: {
+//value: '#ff963b',
+//duration: 1,
+//delay: (t,i,l) => i*15+150
+//}
+//});
+//}, 50);
 
-  const enterHoverOutFn = () => {
-    clearTimeout(enterTimeout);
-    if( !isActive ) return;
-    isActive = false;
+//const enterHoverOutFn = () => {
+//clearTimeout(enterTimeout);
+//if( !isActive ) return;
+//isActive = false;
 
-    anime.remove(DOM.enterLetters);
-    anime({
-      targets: DOM.enterLetters,
-      delay: (t,i,l) => (l-i-1)*15,
-      translateY: [
-        {value: 10, duration: 150, easing: 'easeInQuad'},
-        {value: [-10,0], duration: 150, easing: 'easeOutQuad'}
-      ],
-      opacity: [
-        {value: 0, duration: 150, easing: 'linear'},
-        {value: 1, duration: 150, easing: 'linear'}
-      ],
-      color: {
-        value: '#ffffff',
-        duration: 1,
-        delay: (t,i,l) => (l-i-1)*15+150
-      }
-    });
-  };
+//anime.remove(DOM.enterLetters);
+//anime({
+//targets: DOM.enterLetters,
+//delay: (t,i,l) => (l-i-1)*15,
+//translateY: [
+//{value: 10, duration: 150, easing: 'easeInQuad'},
+//{value: [-10,0], duration: 150, easing: 'easeOutQuad'}
+//],
+//opacity: [
+//{value: 0, duration: 150, easing: 'linear'},
+//{value: 1, duration: 150, easing: 'linear'}
+//],
+//color: {
+//value: '#ffffff',
+//duration: 1,
+//delay: (t,i,l) => (l-i-1)*15+150
+//}
+//});
+//};
 
-  init();
-}
+//init();
+/*}*/
